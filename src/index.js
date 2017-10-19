4fe9a251d5d17e5713153acacff197c35a32bc95
Simen Bekkhus <sbekkhus91@gmail.com>
2017-10-19 10:59:56 +0200

    Make pretty

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { render } from "react-dom";
+import { render } from 'react-dom';
 
-import Presentation from "./presentation";
+import Presentation from './presentation';
 import registerServiceWorker from './registerServiceWorker';
 
 const root = document.getElementById('root');
 
-render(<Presentation/>, root);
+render(<Presentation />, root);
 
 if (module.hot) {
   if (module.hot) {
